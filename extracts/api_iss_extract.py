ab2c71ac7771f24e6f85a8eacb3602919f47297f
root <root@yellow-sub-linux.1vu33pe2jkme5d5bg5qobfwgnf.zx.internal.cloudapp.net>
2020-01-29 17:40:35 +0000

    Initial packages import

--- extracts/api_iss_extract.py
+++ extracts/api_iss_extract.py
@@ -1,1 +1,11 @@
-#Python script to extract data from the ISS API's and make the JSON file readable
+#Using API's to extract sample datasets and store them in a DB
+
+#Importing the key libraries to process unstructured data and store them in a dataframe
+import pandas as pd
+import numpy as np
+import requests
+import json
+
+url = "https://wind-bow.glitch.me/twitch-api/channels/freecodecamp"
+JSONContent = requests.get(url).json()
+print(JSONContent)
