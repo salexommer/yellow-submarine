a63380d3aa174a64df36de66cd9ea93dc75b0ab4
root <root@yellow-sub-linux.1vu33pe2jkme5d5bg5qobfwgnf.zx.internal.cloudapp.net>
2020-02-18 08:56:46 +0000

    NYT extract loop completed

--- extracts/api_nytimes_extract.py
+++ extracts/api_nytimes_extract.py
@@ -9,20 +9,20 @@ import pyjq
 
 #Store the twitch api url and retrieve the raw data
 api_key = 'gcpa6XL4z1zLwQXSxW1Y4HniG3zWCWNM'
-url = 'https://api.nytimes.com/svc/archive/v1/2020/1.json?&api-key='+api_key
+url = 'https://api.nytimes.com/svc/archive/v1/2018/1.json?&api-key='+api_key
 
-#Check the total number of articles in the data extract
-JSONdataheader = pyjq.all('.copyright',JSONdata) 
-JSONdoccount = pyjq.all('.response .docs | length',JSONdata)[0]
-
+        
 #now let's extract the relevant fields only
 JSONdataextract = f'.response .docs [] | {{the_snippet: .snippet, the_headline: .headline .main, the_date: .pub_date, the_news_desk: .news_desk}}'
 JSONdataextractoutput = pyjq.all(JSONdataextract, JSONdata)
-#JSONdataextractoutput_dump = json.dumps(JSONdataextractoutput,indent=4)
+JSONdataextractoutput_dump = json.dumps(JSONdataextractoutput,indent=4)
 #print(JSONdataextractoutput_dump)
 #print("The total number of documents is " + str(JSONdoccount))
 
 #creating a json extract and keeping it locally
+with open('/home/salexommer/Documents/yellow-submarine/extracts/nyt_month_data.json','w') as fi:
+    fi.write(JSONdataextractoutput_dump)
+df = pd.read_json(jsonurl, orient='columns')
 jsonurl = '/home/salexommer/Documents/yellow-submarine/extracts/nyt_month_data.json'
 df = pd.read_json(jsonurl, orient='columns')
 
--- extracts/api_nytimes_extract_v2.py
+++ extracts/api_nytimes_extract_v2.py
@@ -11,15 +11,19 @@ today = date.today()
 m1 = int(today.strftime("%m")) #current month
 y1 = int(today.strftime("%Y")) #current year
 y2 = y1 - 2 #two years ago from today
+y3 = int(today.strftime("%Y")) #current year
 
 #Store the twitch api url and retrieve the raw data
 api_key = 'gcpa6XL4z1zLwQXSxW1Y4HniG3zWCWNM'
+csv_path = '/home/salexommer/Documents/yellow-submarine/extracts/'
 
 #create a loop to cycle through years
 while y1 >= y2:
-    m2 = m1 #variable month for the loop
-    print("Extracting two years worth of data from NYT")
-
+    if y1 == y3:
+        m2 = m1 #variable month for the loop
+    else:
+        m2 = 12
+    print("Extracting two years worth of data from NYT for year "+str(y1))
     #create a loop to cycle through months
     while m2 >= 1:
         print("...extracting the data for " + str(m2) + "/"+ str(y1))
@@ -31,14 +35,16 @@ while y1 >= y2:
         JSONdataextractoutput = pyjq.all(JSONdataextract, JSONdata)
         JSONdataextractoutput_dump = json.dumps(JSONdataextractoutput,indent=4)
         #creating a json extract and keeping it locally
-        with open('/home/salexommer/Documents/yellow-submarine/extracts/nyt_month_data_'+str(m2)+str(y1)+'.json','w') as fi:
+        with open(csv_path+'nyt_month_data_'+str(m2)+str(y1)+'.json','w') as fi:
             fi.write(JSONdataextractoutput_dump)
-        jsonurl = '/home/salexommer/Documents/yellow-submarine/extracts/nyt_month_data_'+str(m2)+str(y1)+'.json'
+        jsonurl =csv_path+'nyt_month_data_'+str(m2)+str(y1)+'.json'
         df = pd.read_json(jsonurl, orient='columns')
         #re-transforming the json into a csv table
-        export_csv = df.to_csv(path_or_buf='/home/salexommer/Documents/yellow-submarine/extracts/nyt_snippet_sample_'+str(m2)+str(y1)+'.csv',index=True)
+        export_csv = df.to_csv(path_or_buf=csv_path+'nyt_snippet_sample_'+str(m2)+str(y1)+'.csv',index=True)
         print("The total number of extracted NYT snippets stored in this csv for "+ str(m2) + "/"+ str(y1)+ ' is ' + str(JSONdoccount))
         m2 = m2 - 1
-    y1 = y1 - 1
-    m1 = 12
-print("Snippets for the past 2 years have been extracted.")
+    else:
+        y1 = y1 - 1
+        #m2 = 12
+else:
+    print("Snippets for the past 2 years have been extracted.")
